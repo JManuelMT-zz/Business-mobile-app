6c7adf0c2d09edc764c069aab7394a955624beda
Jose Manuel Mendez <josemmendezt@gmail.com>
2020-04-21 19:21:30 -0600

    Updates

---
+++ precache-manifest.ec0e29fac613a02e480946966361d865.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e1a698b3b9be962cc536f717daec8b",
+    "url": "/Business-mobile-app/index.html"
+  },
+  {
+    "revision": "ade1b8355cfc317bf042",
+    "url": "/Business-mobile-app/static/css/51.7a8853e6.chunk.css"
+  },
+  {
+    "revision": "183eb9e4629b0ff9a7f6",
+    "url": "/Business-mobile-app/static/css/main.a4b23d7c.chunk.css"
+  },
+  {
+    "revision": "b6afd72083f6eded82d4",
+    "url": "/Business-mobile-app/static/js/0.d0385e25.chunk.js"
+  },
+  {
+    "revision": "a8c4e647e15d4478bd83",
+    "url": "/Business-mobile-app/static/js/1.ec81532a.chunk.js"
+  },
+  {
+    "revision": "6c18e87d595506bfd0c7",
+    "url": "/Business-mobile-app/static/js/2.b4f18df0.chunk.js"
+  },
+  {
+    "revision": "f9ae68daaea71a77a5b0",
+    "url": "/Business-mobile-app/static/js/3.2f668b9d.chunk.js"
+  },
+  {
+    "revision": "fd27d940677ddc4c2b83",
+    "url": "/Business-mobile-app/static/js/4.1393f52a.chunk.js"
+  },
+  {
+    "revision": "1941fdffe846be298b4a",
+    "url": "/Business-mobile-app/static/js/5.0090812e.chunk.js"
+  },
+  {
+    "revision": "ade1b8355cfc317bf042",
+    "url": "/Business-mobile-app/static/js/51.fef6edf6.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/Business-mobile-app/static/js/51.fef6edf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "479f5b15130b9176e8b8",
+    "url": "/Business-mobile-app/static/js/52.1a4c8eeb.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/Business-mobile-app/static/js/52.1a4c8eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1689334ca8e5687c86f0",
+    "url": "/Business-mobile-app/static/js/53.6342b390.chunk.js"
+  },
+  {
+    "revision": "629ad84c1c879e08ffc5",
+    "url": "/Business-mobile-app/static/js/54.557f8ba4.chunk.js"
+  },
+  {
+    "revision": "0f7a446af0c43413005e",
+    "url": "/Business-mobile-app/static/js/55.fe3768d5.chunk.js"
+  },
+  {
+    "revision": "cf4fe9ddbd92688aeecc",
+    "url": "/Business-mobile-app/static/js/56.447989b9.chunk.js"
+  },
+  {
+    "revision": "d6e815163bd0ece329e3",
+    "url": "/Business-mobile-app/static/js/57.9cb7144f.chunk.js"
+  },
+  {
+    "revision": "3f35564ed4eb7cad2498",
+    "url": "/Business-mobile-app/static/js/58.cf1fd99d.chunk.js"
+  },
+  {
+    "revision": "0b9bdfd6581b9b5aba2d",
+    "url": "/Business-mobile-app/static/js/59.24ca9450.chunk.js"
+  },
+  {
+    "revision": "a282400630d48b20b4c6",
+    "url": "/Business-mobile-app/static/js/60.9a89ab62.chunk.js"
+  },
+  {
+    "revision": "930b0b521ad0cca0826b",
+    "url": "/Business-mobile-app/static/js/61.195ada3e.chunk.js"
+  },
+  {
+    "revision": "7378ba3101ec4279e904",
+    "url": "/Business-mobile-app/static/js/62.9f130912.chunk.js"
+  },
+  {
+    "revision": "da0aad2684c6a807c616",
+    "url": "/Business-mobile-app/static/js/63.f1985636.chunk.js"
+  },
+  {
+    "revision": "5b20e0eeba4d62901431",
+    "url": "/Business-mobile-app/static/js/64.a729f33e.chunk.js"
+  },
+  {
+    "revision": "79f698b67a3dfe5afe28",
+    "url": "/Business-mobile-app/static/js/65.021ab15b.chunk.js"
+  },
+  {
+    "revision": "89e75763eeb2b47988c9",
+    "url": "/Business-mobile-app/static/js/66.c8a493c4.chunk.js"
+  },
+  {
+    "revision": "35df87b95c0546e3897b",
+    "url": "/Business-mobile-app/static/js/67.9b2b5a85.chunk.js"
+  },
+  {
+    "revision": "910369845225e78e162e",
+    "url": "/Business-mobile-app/static/js/68.b979b4a2.chunk.js"
+  },
+  {
+    "revision": "66733599aaec4676680e",
+    "url": "/Business-mobile-app/static/js/69.4322c788.chunk.js"
+  },
+  {
+    "revision": "7f25e454eacd122bc0a7",
+    "url": "/Business-mobile-app/static/js/70.3cb76387.chunk.js"
+  },
+  {
+    "revision": "9b03c7b99a33368b8b3d",
+    "url": "/Business-mobile-app/static/js/71.2fda75f2.chunk.js"
+  },
+  {
+    "revision": "fef7f7ba09d0e97b1433",
+    "url": "/Business-mobile-app/static/js/72.e2d9ef97.chunk.js"
+  },
+  {
+    "revision": "c873f7949f2e3766ea8a",
+    "url": "/Business-mobile-app/static/js/73.e5a9b56e.chunk.js"
+  },
+  {
+    "revision": "310ae77b0f3b724613c4",
+    "url": "/Business-mobile-app/static/js/74.c1137c9e.chunk.js"
+  },
+  {
+    "revision": "156fe82ab7a252490891",
+    "url": "/Business-mobile-app/static/js/75.fef164a0.chunk.js"
+  },
+  {
+    "revision": "ebfb2dc3ce4a5f39cd2a",
+    "url": "/Business-mobile-app/static/js/76.ffed4af2.chunk.js"
+  },
+  {
+    "revision": "37be530b4b8e43bd9f70",
+    "url": "/Business-mobile-app/static/js/77.413b5597.chunk.js"
+  },
+  {
+    "revision": "0d96b725504186bff96c",
+    "url": "/Business-mobile-app/static/js/78.0ca8dbd9.chunk.js"
+  },
+  {
+    "revision": "00b482314654c1df5c32",
+    "url": "/Business-mobile-app/static/js/79.e8376c4c.chunk.js"
+  },
+  {
+    "revision": "b5bbd56f77322d51eea1",
+    "url": "/Business-mobile-app/static/js/80.fb72ce5e.chunk.js"
+  },
+  {
+    "revision": "8795e574f438b714481b",
+    "url": "/Business-mobile-app/static/js/81.a1d6e8cc.chunk.js"
+  },
+  {
+    "revision": "6edf956019e87064b7c2",
+    "url": "/Business-mobile-app/static/js/82.b30e9cdd.chunk.js"
+  },
+  {
+    "revision": "c8e38680a9170b42e4d7",
+    "url": "/Business-mobile-app/static/js/83.78bb1562.chunk.js"
+  },
+  {
+    "revision": "f1fc1dce18b3c94b0334",
+    "url": "/Business-mobile-app/static/js/84.9420f5df.chunk.js"
+  },
+  {
+    "revision": "59c9ba41a6730035ce6b",
+    "url": "/Business-mobile-app/static/js/85.edeacffe.chunk.js"
+  },
+  {
+    "revision": "e8c2ecb6ade5bd147bc3",
+    "url": "/Business-mobile-app/static/js/86.6ed13aba.chunk.js"
+  },
+  {
+    "revision": "fb3ba616d6fa3e965f65",
+    "url": "/Business-mobile-app/static/js/87.a8560ff8.chunk.js"
+  },
+  {
+    "revision": "ba0c96258bf680d5e9e1",
+    "url": "/Business-mobile-app/static/js/88.e3187a84.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/Business-mobile-app/static/js/88.e3187a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2011b381f39949ce396c",
+    "url": "/Business-mobile-app/static/js/89.5aa38bcd.chunk.js"
+  },
+  {
+    "revision": "890ef88f93cee1bf916c",
+    "url": "/Business-mobile-app/static/js/90.ee283d0b.chunk.js"
+  },
+  {
+    "revision": "0c2b43b05c2db83f1dfa",
+    "url": "/Business-mobile-app/static/js/91.adfe5696.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/Business-mobile-app/static/js/91.adfe5696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183eb9e4629b0ff9a7f6",
+    "url": "/Business-mobile-app/static/js/main.57e038a3.chunk.js"
+  },
+  {
+    "revision": "dc4a512e5b7f45f77ad0",
+    "url": "/Business-mobile-app/static/js/runtime-main.933ed7cc.js"
+  },
+  {
+    "revision": "7db16f20f5a75ee4292a",
+    "url": "/Business-mobile-app/static/js/stencil-ion-avatar_3-ios-entry-js.79a53c67.chunk.js"
+  },
+  {
+    "revision": "226c5b9b7290fb0a1fdb",
+    "url": "/Business-mobile-app/static/js/stencil-ion-avatar_3-md-entry-js.9a9f21fa.chunk.js"
+  },
+  {
+    "revision": "f3ab89d92652a359124b",
+    "url": "/Business-mobile-app/static/js/stencil-ion-back-button-ios-entry-js.440cb19b.chunk.js"
+  },
+  {
+    "revision": "c458ae1cc0f354ab05fd",
+    "url": "/Business-mobile-app/static/js/stencil-ion-back-button-md-entry-js.9f0ccfec.chunk.js"
+  },
+  {
+    "revision": "955311665bccf2b161f5",
+    "url": "/Business-mobile-app/static/js/stencil-ion-backdrop-ios-entry-js.383c75f9.chunk.js"
+  },
+  {
+    "revision": "89f3ade4755ff2a1dba0",
+    "url": "/Business-mobile-app/static/js/stencil-ion-backdrop-md-entry-js.4902a769.chunk.js"
+  },
+  {
+    "revision": "d3385c53b362efcc3a3e",
+    "url": "/Business-mobile-app/static/js/stencil-ion-card_5-ios-entry-js.e2999a22.chunk.js"
+  },
+  {
+    "revision": "55b3b1b6ce96ead07291",
+    "url": "/Business-mobile-app/static/js/stencil-ion-card_5-md-entry-js.9a743bc1.chunk.js"
+  },
+  {
+    "revision": "67c080f4f345536480bc",
+    "url": "/Business-mobile-app/static/js/stencil-ion-checkbox-ios-entry-js.88a61ef9.chunk.js"
+  },
+  {
+    "revision": "c4a82edf3032369bab4e",
+    "url": "/Business-mobile-app/static/js/stencil-ion-checkbox-md-entry-js.3cb7982c.chunk.js"
+  },
+  {
+    "revision": "aebe8e52d4d2db62888c",
+    "url": "/Business-mobile-app/static/js/stencil-ion-chip-ios-entry-js.ed99f182.chunk.js"
+  },
+  {
+    "revision": "2d3ff5ebe86c2958574a",
+    "url": "/Business-mobile-app/static/js/stencil-ion-chip-md-entry-js.12a3ce40.chunk.js"
+  },
+  {
+    "revision": "16520e4fb8e05129289d",
+    "url": "/Business-mobile-app/static/js/stencil-ion-col_3-entry-js.5f7b0739.chunk.js"
+  },
+  {
+    "revision": "5573fa7bf7fb92fa3a99",
+    "url": "/Business-mobile-app/static/js/stencil-ion-fab_3-ios-entry-js.dd8b2224.chunk.js"
+  },
+  {
+    "revision": "1f15b8588d6835c9a0b7",
+    "url": "/Business-mobile-app/static/js/stencil-ion-fab_3-md-entry-js.5f5ca933.chunk.js"
+  },
+  {
+    "revision": "4b7700e889b9912e6652",
+    "url": "/Business-mobile-app/static/js/stencil-ion-img-entry-js.90e0dd28.chunk.js"
+  },
+  {
+    "revision": "cf67007424353ddc5639",
+    "url": "/Business-mobile-app/static/js/stencil-ion-infinite-scroll_2-ios-entry-js.c7f5e2e6.chunk.js"
+  },
+  {
+    "revision": "4cc6306fc58a17cf216d",
+    "url": "/Business-mobile-app/static/js/stencil-ion-infinite-scroll_2-md-entry-js.3adde59a.chunk.js"
+  },
+  {
+    "revision": "378e108d3c0ecfeb9d02",
+    "url": "/Business-mobile-app/static/js/stencil-ion-input-ios-entry-js.e8fb269d.chunk.js"
+  },
+  {
+    "revision": "06aea17e731e79a2bb99",
+    "url": "/Business-mobile-app/static/js/stencil-ion-input-md-entry-js.4e298bf8.chunk.js"
+  },
+  {
+    "revision": "12e14ea87b711b749589",
+    "url": "/Business-mobile-app/static/js/stencil-ion-loading-ios-entry-js.37a1afe3.chunk.js"
+  },
+  {
+    "revision": "c96d1795cbb4a5ba03d6",
+    "url": "/Business-mobile-app/static/js/stencil-ion-loading-md-entry-js.51fea39b.chunk.js"
+  },
+  {
+    "revision": "c9e99287a3b884f149e7",
+    "url": "/Business-mobile-app/static/js/stencil-ion-popover-ios-entry-js.207bf81c.chunk.js"
+  },
+  {
+    "revision": "fc34375e38e48176c918",
+    "url": "/Business-mobile-app/static/js/stencil-ion-popover-md-entry-js.de37cfb0.chunk.js"
+  },
+  {
+    "revision": "26fb463124dfdb2fbbb8",
+    "url": "/Business-mobile-app/static/js/stencil-ion-progress-bar-ios-entry-js.af574353.chunk.js"
+  },
+  {
+    "revision": "e46f0e5e9323f9de2309",
+    "url": "/Business-mobile-app/static/js/stencil-ion-progress-bar-md-entry-js.315c4f3f.chunk.js"
+  },
+  {
+    "revision": "a5ad001a6960f800a916",
+    "url": "/Business-mobile-app/static/js/stencil-ion-radio_2-ios-entry-js.0f708744.chunk.js"
+  },
+  {
+    "revision": "a44dd0cbec8665cb9763",
+    "url": "/Business-mobile-app/static/js/stencil-ion-radio_2-md-entry-js.3d3e11af.chunk.js"
+  },
+  {
+    "revision": "fd157a3f3e5b113c752f",
+    "url": "/Business-mobile-app/static/js/stencil-ion-reorder_2-ios-entry-js.ceacd74e.chunk.js"
+  },
+  {
+    "revision": "e42b95c2e9aa0e707c11",
+    "url": "/Business-mobile-app/static/js/stencil-ion-reorder_2-md-entry-js.a764162c.chunk.js"
+  },
+  {
+    "revision": "53d8206853b690cbe742",
+    "url": "/Business-mobile-app/static/js/stencil-ion-ripple-effect-entry-js.48e715dd.chunk.js"
+  },
+  {
+    "revision": "82a29e81ea94d2d5e374",
+    "url": "/Business-mobile-app/static/js/stencil-ion-spinner-entry-js.8116b8f8.chunk.js"
+  },
+  {
+    "revision": "83c27991229f6ee527a8",
+    "url": "/Business-mobile-app/static/js/stencil-ion-split-pane-ios-entry-js.448cbb95.chunk.js"
+  },
+  {
+    "revision": "6ebd6a1afbc4b3494b14",
+    "url": "/Business-mobile-app/static/js/stencil-ion-split-pane-md-entry-js.7302fbcc.chunk.js"
+  },
+  {
+    "revision": "23d0c76733d6107bde72",
+    "url": "/Business-mobile-app/static/js/stencil-ion-tab-bar_2-ios-entry-js.5afecb6b.chunk.js"
+  },
+  {
+    "revision": "09e0e00f8b1a37429bbe",
+    "url": "/Business-mobile-app/static/js/stencil-ion-tab-bar_2-md-entry-js.c13507fa.chunk.js"
+  },
+  {
+    "revision": "6d0c55a9ae0e52aff5f8",
+    "url": "/Business-mobile-app/static/js/stencil-ion-tab_2-entry-js.16a94f5f.chunk.js"
+  },
+  {
+    "revision": "14228940ccd52a9b1f33",
+    "url": "/Business-mobile-app/static/js/stencil-ion-text-entry-js.c5e4fcc8.chunk.js"
+  },
+  {
+    "revision": "9571cd137a0baab8b358",
+    "url": "/Business-mobile-app/static/js/stencil-ion-textarea-ios-entry-js.6015e7af.chunk.js"
+  },
+  {
+    "revision": "572b008a09fc309c75a0",
+    "url": "/Business-mobile-app/static/js/stencil-ion-textarea-md-entry-js.e8d634fd.chunk.js"
+  },
+  {
+    "revision": "6fa0dc1b6017a545f1d8",
+    "url": "/Business-mobile-app/static/js/stencil-ion-toggle-ios-entry-js.9085fa86.chunk.js"
+  },
+  {
+    "revision": "a144e6d607c0cab4d719",
+    "url": "/Business-mobile-app/static/js/stencil-ion-toggle-md-entry-js.2bb47ea8.chunk.js"
+  },
+  {
+    "revision": "065990b30e7ce85094ff",
+    "url": "/Business-mobile-app/static/js/stencil-ion-virtual-scroll-entry-js.46b35026.chunk.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47e3ed6c842020f7c85369131a6aaf65.js"
+  "/Business-mobile-app/precache-manifest.ec0e29fac613a02e480946966361d865.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Business-mobile-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
